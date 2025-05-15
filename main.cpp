f9b644aeac7d6fd771c19a2f40e7972fd59c3e55
Aniket Saini <154746704+aniketsaini5@users.noreply.github.com>
2025-05-15 13:23:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hello, TeamCode Hub!" << std::endl;
+    int a = 9;
+    int b= 9;
+    
+    std::cout << "Hello, TeamCode Hub!" <<b+a<< std::endl;
     return 0;
 }
